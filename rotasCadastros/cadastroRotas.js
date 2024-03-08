0c3156d1fa3692ac4d1b295faa5f7af44ab32bfd
Emanoel Silva <emanoelsilva.jua@gmail.com>
2024-03-08 15:10:18 -0300

    criando rotas

---
+++ rotasCadastros/cadastroRotas.js
@@ -0,0 +1,131 @@
+const express =  require('express'); // comando que  permite utilizar o framework do Express
+const routerCadastro = express(); // o express é chamado para criar a aplicação
+
+//routerCadastro.use(bodyParser.json())
+
+routerCadastro.use(express.json())
+routerCadastro.use(express.urlencoded({ extended: true })) //está função está sendo usada para permitir que os dados sejam enviados através app.use
+
+
+
+let cadastros = [
+    { nome: 'Joao', idade: 25, cidade: "Juazeiro do Norte" },
+    { nome: 'Maria', idade: 30, cidade: "Crato" },
+    { nome: 'Pedro', idade: 18, cidade: "Barbalha" },
+    { nome: 'Ana', idade: 25 , cidade: "Juazeiro do Norte"},
+    { nome: 'Carlos', idade: 30 , cidade: "Iguatu"},
+    { nome: 'Julia', idade: 18 , cidade: "Pau dos ferros"},
+    { nome: 'Mariana', idade: 25 , cidade: "Barbalha"},
+    { nome: 'Lucas', idade: 18 , cidade: "Missão Velha"},
+    { nome: 'Beatriz', idade: 30 , cidade: "Crato"},
+    { nome: 'Rodrigo', idade: 25 , cidade: "Iguatu"},
+    { nome: 'Fernanda', idade: 30 , cidade: "Barbalha"},
+    { nome: 'Gustavo', idade: 18 , cidade: "Juazeiro do Norte"},
+    { nome: 'Amanda', idade: 25 , cidade: "Natal"},
+    { nome: 'Rafael', idade: 18 , cidade: "Missão Velha"},
+    { nome: 'Larissa', idade: 30 , cidade: "Juazeiro do Norte"},
+    { nome: 'Diego', idade: 25 , cidade: "Barbalha"},
+    { nome: 'Camila', idade: 18 , cidade: "Natal"},
+    { nome: 'Guilherme', idade: 30 , cidade: "Juazeiro do Norte"},
+    { nome: 'Isabela', idade: 25 , cidade: "Natal"},
+    { nome: 'Vinicius', idade: 18 , cidade: "Crato"}
+
+]
+
+// criado a rota  para o metodo get e passando como parametro a funcao callback que vai retornar os dados da variavel 
+routerCadastro.get('/cadastros/todos', (req,res) =>{
+    console.log(req)
+    res.json(cadastros)
+    })
+
+
+// get  - buscar informações do recurso.
+routerCadastro.get('/cadastro/:id', (req, res) =>{// :id  é um parametro dinamico que vai ser substituido pelo valor da urlapp.use(express.json()); 
+    let id = req.params.id;//  isso faz  com que o parametro seja passado na url e possa ser usado no codigo
+    
+   if(isNaN(id)){ // verifica se é um numero Ex: se  a variavel id for uma string vazia ou null
+       return res.send({message:("Id invalido")}); // tráz a resposta com invalido
+   }
+
+   let indice = parseInt(id); // isso transforma o paramentro em int para poder usar como index do array
+
+   if(indice < 0 || indice >= cadastros.length){ // caso  o indice for menor ou maior que zero ou maior q o tamanho do array
+       return res.send({message:"Indice invalido"})
+   }
+
+   return res.json(cadastros[indice]);
+
+});
+
+
+
+//post  - adicionar uma informação ao recurso.
+routerCadastro.post('/cadastro', (req,res)=>{
+    console.log(req.body)
+    let novoCadastro = req.body; // o let pega  os dados vindo da requisição , o body pega todos os dados que estão sendo enviados por meio dele
+    
+    const existeUsuario = cadastros.some((usuario)=> usuario.nome.toLowerCase() === novoCadastro.nome.toLowerCase())
+
+    if (existeUsuario) {
+        return res.status(404).send({message:"Usuário já existe na base de dados"})
+        
+    }
+
+    cadastros.push(novoCadastro);// add o novo cadastro ao final do vetor
+    console.log(cadastros)
+    return res.json(cadastros); // retornando o array  de todos os cadastros
+
+    //Lembrar de vaildar quando o cadastro que for  passada já exista no array
+})
+
+// Rota que filtro por idade
+
+routerCadastro.get('/cadastros/idade/:idade', (req, res)=>{
+    const parametroIdade = parseInt(req.params.idade);  // converte para inteiro
+    
+    const filtradosPorIdade = cadastros.filter((dado)=> dado.idade === parametroIdade) // retorna um vetor com as pessoas que a mesma idade
+
+    if (filtradosPorIdade.length === 0) { //Validar se a idade pesquisada esta no array
+        return res.status(404).json({message:"Nenhum cadastro encontrado"}) // retorna o erro 
+    }
+
+    return res.json(filtradosPorIdade) // retorna  um json com os cadastros daquela idade
+
+
+})
+
+
+//rota para buscar por nome
+
+routerCadastro.get('/cadastros/nome/:nome', (req, res)=>{
+    const {nome} = req.params;
+
+    const filtrandoPorNome = cadastros.filter((NomePessoa)=> NomePessoa.nome.toLowerCase() === nome.toLowerCase()) // retorna um vetor com as pessoas que existe
+
+    if(filtrandoPorNome.length === 0){  // caso o nome pesquisado não exita  no array ele retorna esse erro
+        return res.status(404).send({message:"Nome não encontrado"})
+    }
+
+    return res.json(filtrandoPorNome)
+})
+
+// rota de filtrar por cidades
+routerCadastro.get('/cadastros/cidades/:cidade', (req,res)=> {
+    
+    const {cidade} = req.params;
+
+    const filtroCidades = cadastros.filter((NomeCidade)=> NomeCidade.cidade.toLowerCase() === cidade.toLowerCase()) // trazendo as informações das cidades que são iguais a que foi pesquisadas
+
+    if (filtroCidades.length === 0) { // quando a cidade  não for encontrada retorna mensagem de erro a busca é feito pelo tamanho de array
+        return res.status(404).send({message:"Cidade não encotrada"});
+    }
+    res.json(filtroCidades);
+
+ })
+
+
+//http://localhost:3000/cadastro/1
+//routerCadastro.listen(3001 , ()=> console.log("Servidor iniciado"));
+
+module.exports = routerCadastro;
+
---
+++ rotasProfessor/professorRotas.js
@@ -0,0 +1,97 @@
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const profRotas = express();
+
+
+profRotas.use(express.json())
+profRotas.use(bodyParser.urlencoded({extended:true}))
+
+// profRotas.use(bodyParser.json())
+// profRotas.use()
+
+
+const professores = [
+    {nome: 'Marcos', area: 'Banco de dados'},
+    {nome: 'Ana', area: 'UX Design'},
+    {nome: 'Hugo', area: 'Programação'},
+    {nome: 'Diego', area: 'Inteligência artificial'},
+    {nome: 'Maria', area: 'Programação'},
+    {nome: 'Hugo', area: 'Engenharia de software'},
+    {nome: 'Danilo', area: 'Programação web e mobile'},
+    {nome: 'Yolanda', area: 'Engenharia de software'},
+]
+
+// essa rota trás todos os professos que estão no array
+profRotas.get('/professor/todos', (req,res)=> {
+    try {
+        res.json(professores)
+    } catch (error) {
+        res.json({error:"Erro ao buscar os professores"})
+    }    
+})
+
+
+// pesquisa um novo professor
+
+profRotas.get('/professor/:nome', (req, res) => {
+
+    try {
+        let {nome} = req.params; // pegando o nome da url para fazer a comparação com o nome do professor
+        let professorEncontrado = professores.filter(nomeprof => nomeprof.nome.toLowerCase() === nome.toLowerCase());
+        
+        if (professorEncontrado.length === 0) {
+            res.status(404).json({ message: `O professor ${nome} não foi encontrado` }) 
+        } else {
+            res.json(professorEncontrado[0])// essa parte  vai retornar um json com o professor encontrado
+        }
+
+        // if(!professorEncontrado) return res.status(404).json({message:'Professor não encontrado'});
+        
+        // // verifique sem o return
+        // return res.json(professorEncontrado[0])  
+        
+    } catch (error) {
+        res.json({error:"Erro ao buscar os professor"})
+    } 
+})
+
+// add um novo professor
+profRotas.post('/professor', (req,res)=> {
+    try {
+        let novaProfessor = req.body; // pega as informações enviadas pelo body na requisição
+        // adicionar um id único para cada professor
+        // const ultimoRegistro = professores [professores.length -1];
+        // novaProfessor.id = ultimoRegistro.id + 1;
+        console.log(novaProfessor);
+
+        let professorExistente = professores.some((prof)=> 
+            
+             prof.nome.toLowerCase() == novaProfessor.nome.toLowerCase()
+         )
+
+
+        if (professorExistente){
+            res.status(404).json ({
+                error: "O usuário já existe"
+            });
+            
+        };
+
+    professores.push(novaProfessor);
+    res.status(201).send({message: "O professor foi criado"});//mandar uma mensagem de sucesso
+    
+    }catch(error){
+        console.log(error);
+         res.status(400).json({ error: 'Não foi possível cadastrar o Professor.' });
+    } 
+   
+
+
+
+})
+
+//http:localhost:3000/professor/
+//profRotas.listen(3000,() => (console.log("servidor rodando")))
+
+module.exports = profRotas;
